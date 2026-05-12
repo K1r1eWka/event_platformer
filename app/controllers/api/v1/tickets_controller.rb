bc1265d8de240f794ec982a6df0aeb3daae5769b
KirilsPoznaks <k.poznaks@gmail.com>
2026-05-12 12:45:53 +0300

    Added auntefication to the tickekController

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::TicketsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_event, only: [ :create, :index ]
   before_action :set_zone, only: [ :create ]
+  before_action :check_ticket_owner, only: [ :update ]
 
   def index
     @tickets = @event.tickets
@@ -17,15 +18,10 @@ class Api::V1::TicketsController < ApplicationController
     end
   end
   def update
-    @ticket = Ticket.find(params[:id])
-    if @ticket.user_id = current_user.id
-      if @ticket.update(ticket_params)
-        render json: @ticket
-      else
-        render json: @ticket.errors, status: :unprocessable_entity
-      end
+    if @ticket.update(ticket_params)
+      render json: @ticket
     else
-      render json: { "error": "permission denied" }, status: :forbidden
+      render json: @ticket.errors, status: :unprocessable_entity
     end
   end
 
@@ -41,5 +37,10 @@ class Api::V1::TicketsController < ApplicationController
 
   def ticket_params
     params.require(:ticket).permit(:status)
+  end
+
+  def check_ticket_owner
+    @ticket = Ticket.find(params[:id])
+    render json: { "error": "permission denied" }, status: :forbidden unless @ticket.user_id == current_user.id
   end
 end
